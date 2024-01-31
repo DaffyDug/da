fab73e0c9eccddb3a053c5c154a853c880fb8654
vaso <vaso@DESKTOP-1IT6A1H>
2024-01-31 18:01:54 +0700

    Добавьте файлы проекта.

---
+++ da/da.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <time.h>
+using namespace std;
+
+void randomarr()
+{
+    int i;
+    int arr[10]={ 0,0,0,0,0,0,0,0,0,0 };
+    if (arr[i] == 0)
+    {
+        arr[i] = rand() % 10;
+        cout << arr[i];i++;
+    }   
+    
+}
+
+int main()
+{
+    randomarr();
+}
